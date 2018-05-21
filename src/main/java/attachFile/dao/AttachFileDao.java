724928712b3cb82903e29444d2b2cb5fa7a7b1be
seokWonYoon <dev.seokwon@gmail.com>
2018-05-21 20:37:54 +0900

    attachFileDao 주석추가 (ddit 202호 pc02)

--- src/main/java/attachFile/dao/AttachFileDao.java
+++ src/main/java/attachFile/dao/AttachFileDao.java
@@ -24,7 +24,7 @@ public class AttachFileDao implements AttachFileDaoInf {
 	}
 	private AttachFileDao(){
 		String resource = "db/mybatis/mybatisConfig.xml";
-		InputStream inputStream = null;
+		InputStream inputStream = null;		//test
 		
 		try {
 			inputStream = Resources.getResourceAsStream(resource);
